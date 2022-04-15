c3e5eef694fbff3bc5e68d706d4352cfb259df5a
codesciousness <codesciousness@gmail.com>
2022-04-14 23:12:32 -0500

    Add media queries to About, Skills and Navbar components.

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -13,16 +13,17 @@ const Navbar = () => {
                     <li><a href="contact.html">CONTACT</a></li>
                 </ul>
             </nav>
-            <nav className="Navbar__tablet Navbar__collapse">
+            <nav className="Navbar__tablet">
                 <a href="index.html"><h1 className="Navbar__title">myCodefolio</h1></a>
-                <ul className="Navbar__links">
+                <ul className="Navbar__links Navbar__collapse">
                     <li><MoreVert fontSize="large" /></li>
                     <li><a href="#portfolio">PORTFOLIO</a></li>
                     <li><a href="about.html">ABOUT</a></li>
                     <li><a href="contact.html">CONTACT</a></li>
                 </ul>
             </nav>
-            <nav className="Navbar__phone Navbar__collapse">
+            <nav className="Navbar__phone">
+                <a href="index.html"><h1 className="Navbar__title">myCodefolio</h1></a>
                 <ul className="Navbar__links">
                     <li><a href="index.html"><Home fontSize="large" /></a></li>
                     <li><a href="about.html"><Person fontSize="large" /></a></li>
